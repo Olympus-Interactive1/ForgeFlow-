63d59781d2e6faaea55542cc51c5a55244fee13e
Olympus Interactive <cigiltepemert@gmail.com>
2026-09-06 15:23:58 +0300

    feat: export built-in providers

---
+++ src/providers/index.ts
@@ -0,0 +1,5 @@
+export { openRouterProvider } from './openrouter.js';
+export { googleProvider } from './google.js';
+export { nvidiaProvider } from './nvidia.js';
+export { falProvider } from './fal.js';
+export { mockProvider } from './mock.js';
